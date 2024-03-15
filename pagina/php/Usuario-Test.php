e8d1863ca4e9974f622e44169de7ce83a9165a62
Carlos Daniel Campos Castro <dannybv7@hotmail.com>
2024-03-15 16:50:53 -0700

    Cambios a Usuario-Test

--- pagina/php/Usuario-Test.php
+++ pagina/php/Usuario-Test.php
@@ -1,15 +1,12 @@
 <?php
 session_start();
-include('../../smarty-master/libs/smarty.class.php');
-include('../smarty/clases/function.class.php');
-$titulo = "Index";
-$smarty=new smarty;
+include('../clases/function.class.php');
 
 // Verificar si el usuario está autenticado
-if (isset($_SESSION['iusuario'])) {
-    header("location:login.php?xd=2");
-    exit; // Detener la ejecución del script después de la redirección
-}
+// if (isset($_SESSION['iusuario'])) {
+//     header("location:login.php?xd=2");
+//     exit; // Detener la ejecución del script después de la redirección
+// }
 $nuevoSingleton = Functions::singleton_functions();
 $iusuario = $_SESSION['iusuario'];
 
@@ -18,61 +15,10 @@ $comprobarMoss = $nuevoSingleton->comprobarMoss($iusuario);
 $comprobarCleaver = $nuevoSingleton->comprobarCleaver($iusuario);
 $comprobarMerril = $nuevoSingleton->comprobarMerril($iusuario);
 $comprobarSjt = $nuevoSingleton->comprobarSjt($iusuario);
-$notificacionexperiencia = $nuevoSingleton->notificacionexperiencia($iusuario);
-$notificacionformacion = $nuevoSingleton->notificacionformacion($iusuario);
-$notificacionaficiones = $nuevoSingleton->notificacionaficiones($iusuario);
-$notificacioninteres = $nuevoSingleton->notificacioninteres($iusuario);
 
 if(isset($_GET['vacante']))
 {
     $vac = $_GET['vacante'];
     header("location:seleccionar_vacantes.php?vacante=$vac");
 }
-if($notificacionexperiencia==0)
-{
-    $COUNTLAB=1;
-
-}
-else 
-{
-    $COUNTLAB=0;
-}
-if($notificacionformacion==0)
-{
-    $COUNFOR=1;
-}
-else 
-{
-    $COUNFOR=0;
-}
-if($notificacionaficiones==0)
-{
-    $COUNTAFI=1;
-}
-else 
-{
-    $COUNTAFI=0;
-}
-if($notificacioninteres==0)
-{
-    $COUNTINT=1;
-}
-else 
-{
-    $COUNTINT=0;
-}
-$COUNT = $COUNTLAB + $COUNFOR + $COUNTAFI + $COUNTINT;
-$smarty->assign("comprobarRaven", $comprobarRaven);
-$smarty->assign("comprobarMoss", $comprobarMoss);
-$smarty->assign("comprobarSjt", $comprobarSjt);
-$smarty->assign("comprobarCleaver", $comprobarCleaver);
-$smarty->assign("comprobarMerril", $comprobarMerril);
-$smarty->assign("titulo", $titulo);
-$smarty->assign("COUNTLAB",$COUNTLAB);
-$smarty->assign("COUNFOR",$COUNFOR);
-$smarty->assign("COUNTAFI",$COUNTAFI);
-$smarty->assign("COUNTINT",$COUNTINT);
-$smarty->assign("COUNT",$COUNT);
-$smarty->assign("iusuario",$iusuario);
-$smarty->display("../smarty/templates/Usuario-Test.tpl");
 ?>
--- pagina/templates/usuario-test.php
+++ pagina/templates/usuario-test.php
@@ -1,3 +1,6 @@
+<?php
+include('../php/Usuario-Test.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,59 +18,38 @@
         <!-- {*Barra de navegacion para Usuarios*} -->
         <?php include("navbar_usuario.php") ?>
 
-        {*Lista de tests*}
+        <!-- Lista de tests -->
         <br>
         <div>
             <div class="row align-items-center" style="margin-left:10px; margin-left:10px">
                 <div class="col">
                     <figure class="text-center">
-                        <input type="image" src="../pagina/img/Ravenimg.jpeg" height="183" width="376" />
+                        <input type="image" src="../../assets/images/LogoTest/Ravenimg.jpeg" height="183" width="376" />
                     </figure>
                     <center>
-                    {if $comprobarRaven >= 1}
-                        <a href="testRaven.php"><button class="btn btn-light" type="button">Iniciar test</button></a>
-                    {else}
-                        <a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>
-                    {/if}  
+                        <?php
+                        if ($comprobarRaven >= 1) {
+                            // Usuario puede iniciar el test
+                            echo '<a href="../templates/testRaven.php"><button class="btn btn-light" type="button">Iniciar test</button></a>';
+                        } else {
+                            // Usuario no puede iniciar el test
+                            echo '<a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>';
+                        }
+                        ?>
                     <center>
                 </div>
                 <div class="col">
                     <figure class="text-center">
-                        <input type="image" src="../pagina/img/Cleaverimg.jpeg" height="183" width="376" />
+                        <input type="image" src="../../assets/images/LogoTest/Cleaverimg.jpeg" height="183" width="376" />
                     </figure>
                     <center>
-                    {if $comprobarCleaver >= 1}
-                        <a href="cleaver.php"><button class="btn btn-light" type="button">Iniciar test</button></a>
-                    {else}
-                        <a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>
-                    {/if}  
-                    <center>
-                </div>
-            </div>
-            <br><br>
-            <div class="row align-items-center" style="margin-left:10px; margin-left:10px">
-                <div class="col">
-                    <figure class="text-center">
-                        <input type="image" src="../pagina/img/Mossimg.jpeg" height="183" width="376" />
-                    </figure>
-                    <center>
-                    {if $comprobarMoss >= 1}
-                        <a href="test_moss.php"><button class="btn btn-light" type="button">Iniciar test</button></a>
-                    {else}
-                        <a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>
-                    {/if}  
-                    <center>
-                </div>
-                <div class="col">
-                    <figure class="text-center">
-                        <input type="image" src="../pagina/img/Merrilimg.jpeg" height="183" width="376" />
-                    </figure>
-                    <center>
-                    {if $comprobarMerril >= 1}
-                        <a href="test_merril.php"><button class="btn btn-light" type="button">Iniciar test</button></a>
-                    {else}
-                        <a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>
-                    {/if}  
+                        <?php
+                        if ($comprobarCleaver >= 1) {
+                            echo '<a href="../templates/cleaver.php"><button class="btn btn-light" type="button">Iniciar test</button></a>';
+                        } else {
+                            echo '<a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>';
+                        }
+                        ?>
                     <center>
                 </div>
             </div>
@@ -75,32 +57,65 @@
             <div class="row align-items-center" style="margin-left:10px; margin-left:10px">
                 <div class="col">
                     <figure class="text-center">
-                        <input type="image" src="../pagina/img/SJTimg.jpeg" height="183" width="376" />
+                        <input type="image" src="../../assets/images/LogoTest/Mossimg.jpeg" height="183" width="376" />
                     </figure>
                     <center>
-                    {if $comprobarSjt >= 1}
-                        <a href="test_sjt.php"><button class="btn btn-light" type="button">Iniciar test</button></a>
-                    {else}
-                        <a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>
-                    {/if}  
+                        <?php
+                        if ($comprobarMoss >= 1) {
+                            echo '<a href="../templates/test_moss.php"><button class="btn btn-light" type="button">Iniciar test</button></a>';
+                        } else {
+                            echo '<a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>';
+                        }
+                        ?>
+                    <center>
+                </div>
+                <div class="col">
+                    <figure class="text-center">
+                        <input type="image" src="../../assets/images/LogoTest/Merrilimg.jpeg" height="183" width="376" />
+                    </figure>
+                    <center>
+                        <?php
+                        if ($comprobarMerril >= 1) {
+                            echo '<a href="../templates/test_merril.php"><button class="btn btn-light" type="button">Iniciar test</button></a>';
+                        } else {
+                            echo '<a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>';
+                        }
+                        ?>
+                    <center>
+                </div>
+            </div>
+            <br><br>
+            <div class="row align-items-center" style="margin-left:10px; margin-left:10px">
+                <div class="col">
+                    <figure class="text-center">
+                        <input type="image" src="../../assets/images/LogoTest/SJTimg.jpeg" height="183" width="376" />
+                    </figure>
+                    <center>
+                        <?php
+                        if ($comprobarSjt >= 1) {
+                            echo '<a href="../templates/test_sjt.php"><button class="btn btn-light" type="button">Iniciar test</button></a>';
+                        } else {
+                            echo '<a href="#"><button class="btn btn-light disabled" type="button">No puedes iniciar este test</button></a>';
+                        }
+                        ?>
                     <center>
                 </div>
             </div>
             <br>
         </div>
 
-        {*Conexion de librerias de JavaScript y bootstrap*}                
+        <!-- Conexion de librerias de JavaScript y bootstrap -->                
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         
-        {* <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.5/push.js"
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.5/push.js"
             integrity="sha512-dzuBh7UxT5g4MmnbR3ybHMK2g2zxGXILXHuLsUwo8XJmoW2JTTqcg4bFFu0RnBO+kPTvKafgVYh8hnCN/l8ijQ=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.5/push.min.js"
             integrity="sha512-1zotA6QprPWXVvgx8KFnvanxTZhm7P/uadmELhEUs3fHYvGDqkYa0ZUc3Q0m+3w7AUcgG5k4rUiFDdSkRJhqaA=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script> *}
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
     </body>
 
 </html>
