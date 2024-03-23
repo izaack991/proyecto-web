7657dec682335a52dcb05d907affc773410c52aa
Jonathan Noriega <jonathannoriega.urias@gmail.com>
2024-03-22 17:53:19 -0700

    quedo responsive

--- pagina/templates/vacante.php
+++ pagina/templates/vacante.php
@@ -54,9 +54,6 @@
 
 <body>
   <script src="../js/ubicacion.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-  </script>
   <!--Barra de navegacion para Empresa-->
   <?php include("navbar_empresa.php") ?>
 
