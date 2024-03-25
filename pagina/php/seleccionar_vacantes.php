2439cea68293c02dc64d8e8eca1cb97e9dbd36d2
fjvazquezg <125910134+fjvazquezg@users.noreply.github.com>
2024-03-24 22:55:12 -0700

    Se agrego la sesion de la id vacante no se llenada

--- pagina/php/seleccionar_vacantes.php
+++ pagina/php/seleccionar_vacantes.php
@@ -99,6 +99,7 @@ $_finduser = Functions::singleton_functions();
 if(isset($_POST['id_vacante']))
 {
     $id_vacante = $_POST['id_vacante'];
+    $_SESSION['id_vacante'] = $id_vacante;
     $resultado = $_finduser->seleccionar_vacantes($id_vacante);
     echo json_encode($resultado);
 }
