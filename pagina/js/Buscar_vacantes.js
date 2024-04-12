6a782bf8f91927f404e6a2258a5fd1fb04e1667a
Luis <luiseduardovelazquezcastro@gmail.com>
2024-04-12 14:26:56 -0700

    SE MODIFICO CARD DE BUSCAR_VACANTES

--- pagina/js/Buscar_vacantes.js
+++ pagina/js/Buscar_vacantes.js
@@ -2,37 +2,39 @@ $(document).ready(function(){
     $.ajax({
         url: "../php/Buscar_vacantes.php",
         type: "POST",
-        dataType: "json", // Especifica el tipo de datos que esperas recibir
+        dataType: "json",
         success: function(data){
-            // Éxito: procesar los datos recibidos
             if (data.length > 0) {
-                // Recorrer el arreglo y agregar tarjetas al contenedor
+                var maxHeight = 0;
                 $.each(data, function(index, vacante){
-                    // Construir la estructura de la tarjeta
                     var card = '<div class="col-lg-4 col-md-6 col-sm-12">';
-                            card += '<div class="card border-primary shadow p-3 mb-5 bg-body rounded">';
-                            card += '<div class="card-body">';
-                            card += '<h4 class="card-title text-danger">' + vacante.puesto + '</h4>';
-                            card += '<h5 class="card-title">' + vacante.empresa + '</h5>';
-                            card += '<h6 class="card-title">' + vacante.region + '</h6>';
-                            card += '<p class="card-text">' + vacante.datos_adicionales + '</p>';
-                            card += '<form action="seleccionar_vacantes.php" method="POST">';
-                            card += '<input type="text" value="'+vacante.id_vacante+'" name="id_vacante" id="id_vacante" hidden>';
-                            card += '<input type="submit" value="Leer más" class="btn btn-primary">';
-                            card += '</form></div></div></div>';
-
-                    // Agregar la tarjeta al contenedor
+                    card += '<div class="card border-primary shadow p-3 mb-5 bg-body rounded">';
+                    card += '<div class="card-body">';
+                    card += '<h4 class="card-title text-danger">' + vacante.puesto + '</h4>';
+                    card += '<h5 class="card-title">' + vacante.empresa + '</h5>';
+                    card += '<h6 class="card-title">' + vacante.region + '</h6>';
+                    card += '<p class="card-text">' + vacante.datos_adicionales + '</p>';
+                    card += '<form action="seleccionar_vacantes.php" method="POST">';
+                    card += '<input type="text" value="'+vacante.id_vacante+'" name="id_vacante" id="id_vacante" hidden>';
+                    card += '<input type="submit" value="Leer más" class="btn btn-primary">';
+                    card += '</form></div></div></div>';
                     $('#vacantesContainer').append(card);
+
+                    // Encontrar la altura máxima
+                    var cardHeight = $('.card').last().height();
+                    if (cardHeight > maxHeight) {
+                        maxHeight = cardHeight;
+                    }
                 });
+
+                // Establecer la altura máxima para todas las tarjetas
+                $('.card').height(maxHeight);
             } else {
-                // No se encontraron vacantes
                 $('#vacantesContainer').html('No se encontraron vacantes.');
             }
         },
         error: function(xhr, status, error){
-            // Error al llamar al script PHP
             console.error(xhr.responseText);
         }
     });
-
 });
