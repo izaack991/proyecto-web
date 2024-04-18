74cf1b943316df8045c814584b06bd6b97e0c2db
KEV1NVAll3 <kevin.vall328@gmail.com>
2024-04-17 17:17:42 -0700

    Correciones en los tres Registros, y validacion de edad en Usuario.php

--- pagina/php/Usuario.php
+++ pagina/php/Usuario.php
@@ -9,22 +9,37 @@ $_compCorreo = Functions::singleton_functions();
 $irol = $_SESSION['rol'];
 //print_r($_SESSION);
 
+// Comprobación de que los campos de contraseña NO estén vacíos
 if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
+
   $_ruta = $_FILES['txtruta'];
   $_cons = $_FILES['txtcons'];
+
+  // Comprobación de que las contraseñas NO sean distintas
   if ($_POST['txt_PASSWORD'] != $_POST['txt_PASSWORD2']) {
-    $alerta = "<script> Swal.fire('¡Las Contraseñas NO coinciden!');</script>";
+
+    // Alerta cuando las contraseñas NO coincidan.
     echo "errorPassword";
     return;
+
   } else {
+    
+    // Comprobación de que la Empresa no llegue con la imagen vacía
     if ($irol == 1 && $_ruta == null) {
-      //$alerta = "<script> Swal.fire('¡No subió la Imagen de la Empresa!');</script>";
+      
+      // Alerta cuando la imagen llega vacía con el rol Empresa
       echo "errorImagenEmpresa";
       return;
+
     } else if ($irol == 1 && $_cons == null) {
+
+      // Alerta cuando la constancia llega vacía
       echo "errorConstancia";
       return;
+
     } else {
+
+      //Asignamos variables con los datos del Usuario
       $f_id_usuario = $_findUser->consec_usuario();
       $_nombre = $_POST['txt_NOMBRE'];
       $_apellido = $_POST['txt_APELLIDOS'];
@@ -37,7 +52,11 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
       $_telefono = $_POST['txt_TELEFONO'];
       $_domicilio = $_POST['txt_DOMICILIO'];
       $_razon = $_POST['txt_razon'];
+
+      //Asignamos el estatus default
       $_status = 0;
+
+      //Generamos el Token para la verificación
       $_token = rand(1000, 9999);
 
       // Enviar el correo electrónico con el token de verificación
@@ -62,22 +81,20 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
       $tamanoCons = $_FILES['txtcons']['size'];
       $tempCons = $_FILES['txtcons']['tmp_name'];
 
+      //Llamamos la función para confirmar que el correo NO esté registrado
       $comp_correo = $_compCorreo->buscar_correo($_correo);
 
       if ($comp_correo == 0) {
 
-        if ($_ruta != null) {
-          //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
-          if (
-            !((strpos($imgTipo, "gif") || strpos($imgTipo, "jpeg") || strpos($imgTipo, "jpg") || strpos($imgTipo, "png") || strpos($imgTipo, "bmp") || strpos($imgTipo, "tiff") || strpos($imgTipo, "webp") || strpos($imgTipo, "svg") || strpos($imgTipo, "raw") || strpos($imgTipo, "psd") || strpos($imgTipo, "ai") || strpos($imgTipo, "eps") || strpos($imgTipo, "pdf") || strpos($imgTipo, "ico") || strpos($imgTipo, "tga") || strpos($imgTipo, "pict") || strpos($imgTipo, "exif") || strpos($imgTipo, "heif") || strpos($imgTipo, "heic") || strpos($imgTipo, "avif") || strpos($imgTipo, "pbm") || strpos($imgTipo, "pgm") || strpos($imgTipo, "ppm") || strpos($imgTipo, "xcf") || strpos($imgTipo, "arw") || strpos($imgTipo, "cr2") || strpos($imgTipo, "nef") || strpos($imgTipo, "orf") || strpos($imgTipo, "rw2") || strpos($imgTipo, "dng") || strpos($imgTipo, "crw") || strpos($imgTipo, "srw") || strpos($imgTipo, "raf") || strpos($imgTipo, "mrw") || strpos($imgTipo, "erf") || strpos($imgTipo, "kdc") || strpos($imgTipo, "mos") || strpos($imgTipo, "nrw") || strpos($imgTipo, "obm") || strpos($imgTipo, "pef") || strpos($imgTipo, "x3f") || strpos($imgTipo, "rawzor") || strpos($imgTipo, "pxr") || strpos($imgTipo, "srf") || strpos($imgTipo, "3fr") || strpos($imgTipo, "qtk") || strpos($imgTipo, "rwz") || strpos($imgTipo, "xbm") || strpos($imgTipo, "wdp") || strpos($imgTipo, "hdp") || strpos($imgTipo, "ktx") || strpos($imgTipo, "kro") || strpos($imgTipo, "hdr") || strpos($imgTipo, "jxr") || strpos($imgTipo, "apng") || strpos($imgTipo, "jp2") || strpos($imgTipo, "jls") || strpos($imgTipo, "jpf") || strpos($imgTipo, "jpm") || strpos($imgTipo, "jpx") || strpos($imgTipo, "bpg") || strpos($imgTipo, "flif")) && ($tamano < 2000000))
-          ) {
+        if ($_ruta != null && $irol == 1) {
 
-            $alerta = "<script> Swal.fire({
-              title: 'Error!',
-              text: 'La extensión o el tamaño de los archivos no es correcta. Solo se permite: .gif, .jpg, .png. y de 200 kb como máximo.',
-              icon: 'error');</script>";
+          //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+          if (!((strpos($imgTipo, "gif") || strpos($imgTipo, "jpeg") || strpos($imgTipo, "jpg") || strpos($imgTipo, "png") || strpos($imgTipo, "bmp") || strpos($imgTipo, "tiff") || strpos($imgTipo, "webp") || strpos($imgTipo, "svg") || strpos($imgTipo, "raw") || strpos($imgTipo, "psd") || strpos($imgTipo, "ai") || strpos($imgTipo, "eps") || strpos($imgTipo, "pdf") || strpos($imgTipo, "ico") || strpos($imgTipo, "tga") || strpos($imgTipo, "pict") || strpos($imgTipo, "exif") || strpos($imgTipo, "heif") || strpos($imgTipo, "heic") || strpos($imgTipo, "avif") || strpos($imgTipo, "pbm") || strpos($imgTipo, "pgm") || strpos($imgTipo, "ppm") || strpos($imgTipo, "xcf") || strpos($imgTipo, "arw") || strpos($imgTipo, "cr2") || strpos($imgTipo, "nef") || strpos($imgTipo, "orf") || strpos($imgTipo, "rw2") || strpos($imgTipo, "dng") || strpos($imgTipo, "crw") || strpos($imgTipo, "srw") || strpos($imgTipo, "raf") || strpos($imgTipo, "mrw") || strpos($imgTipo, "erf") || strpos($imgTipo, "kdc") || strpos($imgTipo, "mos") || strpos($imgTipo, "nrw") || strpos($imgTipo, "obm") || strpos($imgTipo, "pef") || strpos($imgTipo, "x3f") || strpos($imgTipo, "rawzor") || strpos($imgTipo, "pxr") || strpos($imgTipo, "srf") || strpos($imgTipo, "3fr") || strpos($imgTipo, "qtk") || strpos($imgTipo, "rwz") || strpos($imgTipo, "xbm") || strpos($imgTipo, "wdp") || strpos($imgTipo, "hdp") || strpos($imgTipo, "ktx") || strpos($imgTipo, "kro") || strpos($imgTipo, "hdr") || strpos($imgTipo, "jxr") || strpos($imgTipo, "apng") || strpos($imgTipo, "jp2") || strpos($imgTipo, "jls") || strpos($imgTipo, "jpf") || strpos($imgTipo, "jpm") || strpos($imgTipo, "jpx") || strpos($imgTipo, "bpg") || strpos($imgTipo, "flif")) && ($tamano < 2000000))) {
+            
+            //Alerta cuando la imagen NO es valida
             echo "errorImagen";
             return;
+
           } else {
             //Si la imagen es correcta en tamaño y tipo
             //Se intenta subir al servidor
@@ -89,11 +106,8 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
               //Mostramos la imagen subida
               //echo '<p><img src="assets/images/'.$archivo.'"></p>';
             } else {
+
               //Si no se ha podido subir la imagen, mostramos un mensaje de error
-              $alerta = "<script> Swal.fire({
-                title: 'Error!',
-                text: 'No se pudo subir la imagen al sevidor.',
-                icon: 'error');</script>";
               echo "errorImagenServer";
               return;
             }
@@ -104,12 +118,10 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
           //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
           if (!((strpos($conTipo, "jpeg") || strpos($conTipo, "jpg") || strpos($conTipo, "png") || strpos($conTipo, "pdf")) && ($tamanoCons < 20000000))) {
 
-            $alerta = "<script> Swal.fire({
-                title: 'Error!',
-                text: 'La extensión o el tamaño de los archivos no es correcta. Solo se permite: .jpg, .png. y de 200 kb como máximo.',
-                icon: 'error');</script>";
+            //Alerta si la constancia NO es valida
             echo "errorConstanciaTipoTamaño";
             return;
+
           } else {
             //Si la imagen es correcta en tamaño y tipo
             //Se intenta subir al servidor
@@ -121,26 +133,46 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
               //Mostramos la imagen subida
               //echo '<p><img src="assets/images/'.$archivo.'"></p>';
             } else {
+              
               //Si no se ha podido subir la imagen, mostramos un mensaje de error
-              $alerta = "<script> Swal.fire({
-                  title: 'Error!',
-                  text: 'No se pudo subir la imagen al sevidor.',
-                  icon: 'error');</script>";
               echo "errorConsServer";
               return;
+
             }
           }
         }
 
+        //Valida el rol del usuario, si es diferente de Empresa, se asgigna el estatus en 1, sino queda en 0
         if($irol != 1) {
+          
+          //Asignacion del estatus en 1 para el Usuario/Estudiante
           $_status = 1;
+
+          //Validacion de la diferencia de edad entre la fecha actual y la de nacimiento del Usuario/Estudiante
+          $fecha_actual = new DateTime(); // Fecha actual
+          $fecha_de_nacimiento = new DateTime($_fecha_nac);
+          $edad = $fecha_actual->diff($fecha_de_nacimiento); // Diferencia entre fechas
+
+          // Verificar si el usuario tiene 18 años o más
+          if ($edad->y < 18) {
+
+            //Alerta si el Usuari/Estudiante NO es mayor de edad
+            echo "errorEdad";
+            return;
+
+          }
         }
 
         //$f_id_usuario = $_findUser -> consec_usuario();
+        
+        //Guardamos a los usuarios Nuevos
         $newuser = $nuevoUsuario->guardar_usuario($f_id_usuario, $_nombre, $_apellido, $_correo, $_fecha_nac, $_no_identificacion, $_password, $_sexo, $_region, $_telefono, $_domicilio, $irol, $_status, $_ruta, $_cons, $_razon, $_token);
 
       } else {
+
+        //Alerta si el Correo está registrado en la base de datos
         echo "errorCorreoDuplicado";
+
       }
 
     }
