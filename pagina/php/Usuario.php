7ce6a732184e8153c38dce9c9d2f55ee9e23a677
juanluis5826 <ultracore5826@gmail.com>
2024-04-18 17:25:34 -0700

    correccion de usuario.php ruta de la imagen

--- pagina/php/Usuario.php
+++ pagina/php/Usuario.php
@@ -144,6 +144,7 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
       $imgName = 'pfp_' . $f_id_usuario . '.' . $imgExt;
       $tamano = $_FILES['txtruta']['size'];
       $temp = $_FILES['txtruta']['tmp_name'];
+      $ruta_img = '../userfiles/img/' . $imgName;
 
       //Obtenemos algunos datos necesarios sobre el archivo de la constancia
       $conTipo = $_FILES['txtcons']['type'];
@@ -238,7 +239,7 @@ if (isset($_POST['txt_PASSWORD']) && (isset($_POST['txt_PASSWORD2']))) {
         //$f_id_usuario = $_findUser -> consec_usuario();
         
         //Guardamos a los usuarios Nuevos
-        $newuser = $nuevoUsuario->guardar_usuario($f_id_usuario, $_nombre, $_apellido, $_correo, $_fecha_nac, $_no_identificacion, $_password, $_sexo, $_region, $_telefono, $_domicilio, $irol, $_status, $_ruta, $_cons, $_razon, $_token, $_universidad, $_carrera, $_ingreso);
+        $newuser = $nuevoUsuario->guardar_usuario($f_id_usuario, $_nombre, $_apellido, $_correo, $_fecha_nac, $_no_identificacion, $_password, $_sexo, $_region, $_telefono, $_domicilio, $irol, $_status, $ruta_img, $_cons, $_razon, $_token, $_universidad, $_carrera, $_ingreso);
 
       } else {
 
