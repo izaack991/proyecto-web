ff304133cf8d6d584f6a4639c20a42017cd7686c
juandiegosa <juandie_sa@hotmail.com>
2024-04-23 18:05:14 -0700

    cambios en el chat, seleccion de chat

--- pagina/php/conversaciones.php
+++ pagina/php/conversaciones.php
@@ -2,18 +2,27 @@
 session_start();
 include("../clases/function.class.php");
 $buscar = Functions::singleton_functions();
-
 if (isset($_POST['valor'])) {
     $valor = $_POST['valor'];
-
     if ($valor == 1) {
         $id_usuario = $_SESSION['iusuario'];
         $conversacion = $buscar->buscarConversacion($id_usuario);
         echo json_encode($conversacion);
     } elseif ($valor == 2) {
-        $id_usuario = $_SESSION['iusuario'];
-        $id_empresa = 2; // Esto debe ser un número, no una comparación
+    $rol = $_SESSION['rol'];
+    if ($rol == 1 )
+    {
+        $id_usuario = $_POST['id'];
+        $id_empresa = $_SESSION['iusuario'];
         $mensaje = $buscar->buscarMensaje($id_empresa, $id_usuario);
+
+    }
+    else
+    {
+        $id_usuario = $_SESSION['iusuario'];
+        $id_empresa = $_POST['id'];
+        $mensaje = $buscar->buscarMensaje($id_empresa, $id_usuario);
+    }
         echo json_encode($mensaje);
     } else {
         echo json_encode(array('error' => 'El valor proporcionado no es válido'));
@@ -21,5 +30,4 @@ if (isset($_POST['valor'])) {
 } else {
     echo json_encode(array('error' => 'No se proporcionó ningún valor'));
 }
-
 ?>
