b1fb090e206a5baf72e729ede670cb807aab0601
Carlos Daniel Campos Castro <dannybv7@hotmail.com>
2024-05-09 15:09:31 -0700

    Cambio nombre seleccionar vacante

--- pagina/templates/seleccionar_vacantes.php
+++ pagina/templates/seleccionar_vacantes.php
@@ -10,7 +10,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>"Selección de Vacante"</title>
+  <title>Selección de Vacante</title>
   <link id="theme-style" rel="stylesheet" href="../../assets/css/devresume.css">
   <link id="theme-style" rel="stylesheet" href="../../assets/css/theme-1.css">
   <link id="theme-style" rel="stylesheet" href="../../assets/css/styles.css">
